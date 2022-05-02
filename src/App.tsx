7a3068393a68783c8d16e1989135e36be948ac43
Paulo Sérgio <036paulo@gmail.com>
2022-05-02 11:40:27 -0300

    Projeto inicial da rocketseat state 1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Widget } from "./components/Widget";
+
+export function App(){
+  return <Widget />
+}
+// import { useState } from 'react'
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return <h1>hello word</h1>
+// }
+
+// export default App
